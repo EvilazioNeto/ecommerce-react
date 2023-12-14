93a0fe8791522c16d998d4e822a8fb5554cfcfb0
Evilazio Neto <evilazioneto943@gmail.com>
2023-12-14 16:26:51 -0300

    Responsividade do carrinho

--- src/components/Carrinho/FecharPedido/FecharPedido.tsx
+++ src/components/Carrinho/FecharPedido/FecharPedido.tsx
@@ -5,7 +5,7 @@ function FecharPedido({valorTotal}: {valorTotal: number}) {
   return (
     <>
       <div className={styles.comprar}>
-        <h2>Subtotal: ${valorTotal}</h2>
+        <h2 className={styles.subtotal}>Subtotal: ${valorTotal}</h2>
         <button>Fechar Pedido</button>
       </div>
     </>
